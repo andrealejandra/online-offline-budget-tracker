aa8f9aaf9e32052d230f3d968e9dc42da3ed05e5
Andrea Virgen <andrealejandra@andreas-mbp.lan>
2020-08-13 20:58:24 -0500

    working app w/ registered service worker

---
+++ public/service-worker.js
@@ -0,0 +1,56 @@
+const FILES_TO_CACHE = [
+    '/',
+    'style.css',
+    'index.js',
+    'manifest.json',
+    '/icons/icon_192x192.png',
+    '/icons/icon_512x512.png',
+    '/db.js'
+];
+
+const CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
